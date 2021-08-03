d41e42604e0ca5779447c8a2e22b541de61a3a74
qiliangfan <1035847702@qq.com>
2021-08-04 00:11:40 +0800

    insert data

--- backend/network/main/crud.py
+++ backend/network/main/crud.py
@@ -1,8 +1,9 @@
 """
 Database CRUD
 """
-from main.models import Port, Link
+from main.models import Port, Link, Subnet
 from django.db.models import Q
+import ipaddress
 
 class CRUD:
 
@@ -14,3 +15,16 @@ class CRUD:
         links = Link.objects.filter(Q(src_port__id = port.id) | Q(dst_port_id = port.id))
         ports = [link.src_port if link.src_port.id != port.id else link.dst_port for link in links ]
         return ports
+
+    def get_tor(self, ip: str):
+        """
+        get TOR switch
+        """
+        if "." in ip:  # ipv4
+             ip = ".".join(ip.split(".")[:2])
+        elif ":" in ip:
+            ip = ":".join(ip.split(":")[:3])
+        else:
+            raise ValueError()
+
+        subnets = Subnet.objects.filter(Q(ip__startswith = ip) | Q(ip6__starswith = ip))
--- backend/network/main/models.py
+++ backend/network/main/models.py
@@ -1,66 +1,58 @@
 from django.db import models
-from django.db.models.enums import Choices
 from django.db.models.fields import *
 from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
-class Subnet(models.Model):
-
-    class Meta:
-        db_table = "subnet"
-
 
 class Switch(models.Model):
 
-    id = CharField(name='id',max_length=30,null=False,db_index=True,primary_key=True)
-    ip = CharField(name="ip", max_length=30, null=False)
-    ip6 = CharField(name="ipv6", max_length=30, null=True)   # not useful as ip
-    data_center = CharField(name="data_center", max_length=30, null=False)
+    id = CharField(max_length=30,null=False,db_index=True,primary_key=True)
+    ip = CharField(max_length=30, null=False)
+    ip6 = CharField(max_length=30, null=True, )   # not useful as ip
+    data_center = CharField(max_length=30, null=False)
 
-    class Meta:
 
-        db_table = "switch"
+
+class Subnet(models.Model):
+    switch = ForeignKey(to=Switch, on_delete=models.CASCADE, related_name="sw_subnet")
+    ip = CharField(max_length=30)
+    prefix = CharField(max_length=30)
+    ip6 = CharField(max_length=30)
+    prefix6 = CharField(max_length=30)
+
+
+
 
 class Port(models.Model):
     
-    id = IntegerField(name="id",primary_key=True)
-    name = CharField(name="name", max_length=30, null=False)
-    real_name = CharField(name="real_name", max_length=30, null=False)
-    switch_id = ForeignKey(to=Switch, on_delete=models.CASCADE)
-    speed = IntegerField(name="speed", null=False)
-    is_shutdown = IntegerField(name="is_shutdown", null=False)
+    id = IntegerField(primary_key=True)
+    name = CharField(max_length=30, null=False)
+    real_name = CharField(max_length=30, null=False)
+    switch = ForeignKey(to=Switch, on_delete=models.CASCADE, related_name="sw_port")
+    speed = IntegerField(null=False)
+    is_shutdown = IntegerField(null=False)
     
-    class Meta:
-
-        db_table = "port"
-
 
 class Link(models.Model):
-    src_port = ForeignKey(to=Port, on_delete=models.CASCADE)
-    dst_port = ForeignKey(to=Port, on_delete=models.CASCADE)
-    max_speed = IntegerField(name="max_speed", null=False)
+    src_port = ForeignKey(to=Port, on_delete=models.CASCADE, related_name="src_port_link")
+    dst_port = ForeignKey(to=Port, on_delete=models.CASCADE, related_name="dst_port_link")
+    max_speed = FloatField(null=False)
 
-    class Meta:
 
-        db_table = "link"
-    
 class SFlow(models.Model):
 
-    src_ip = CharField(name='srcip',max_length=30,null=False)
-    dst_ip = CharField(name='dstip',max_length=30,null=False)
-    ip_type = CharField(name='ipType',max_length=30,null=False)
-    sw_port = ForeignKey(to=Port,on_delete=models.CASCADE)
-    src_dc = CharField(name='srcDc',max_length=30,null=False)
-    dst_dc = CharField(name='dstDc',max_length=30,null=False)
-    src_psm = CharField(name='srcPsm',max_length=30,null=False)
-    dst_psm = CharField(name='dstPsm',max_length=30,null=False)
-    bytes = IntegerField(name='bytes',null=False)
+    src_ip = CharField(max_length=30,null=False)
+    dst_ip = CharField(max_length=30,null=False)
+    ip_type = CharField(max_length=30,null=False)
+    sw_port = ForeignKey(to=Port,on_delete=models.CASCADE, related_name="port_sflow")
+    src_dc = CharField(max_length=30,null=False)
+    dst_dc = CharField(max_length=30,null=False)
+    src_psm = CharField(max_length=30,null=False)
+    dst_psm = CharField(max_length=30,null=False)
+    bytes = FloatField(null=False)
     
     class Dir(models.IntegerChoices):
         dir_1=1
         dir_0=0
-    dir = IntegerField(name='dir',choices=Dir.choices)     
-    class Meta:
-
-        db_table = "sflow"
+    dir = IntegerField(choices=Dir.choices)     
